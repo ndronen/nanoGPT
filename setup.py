b24284310473f220c2179db9452d6276763b7925
Nicholas Dronen <21194+ndronen@users.noreply.github.com>
2023-05-16 08:53:32 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='nanoGPT',
+    version='0.0.1',    
+    description='Experiment',
+    url='https://github.com/ndronen/nanoGPT',
+    author='Andrew Karpathy, Nicholas Dronen',
+    author_email='ndronen@gmail.com',
+    license='BSD 2-clause',
+    packages=[],
+    install_requires=[
+        'torch', 'numpy', 'pytorch'
+    ],
+    classifiers=[]
+)
